b057c2414154a8ff10ee71a7b923896be0968a8d
Baloryx <117108968+Baloryx@users.noreply.github.com>
2023-09-01 18:50:40 +0000

    Actually updated the get_input() function to have a do_lower attribute Fixed menu having integer prices and added some menu items

--- res_library.py
+++ res_library.py
@@ -1,10 +1,11 @@
 #Place to store all extra functions required for the program.
 
-def get_input(prompt, given_type, in_range):
+def get_input(prompt, given_type, in_range, do_lower):
     while True:
         value = input(prompt)
         if type(value) == given_type:
-            pass
+            if do_lower():
+                value = value.lower()
         elif given_type == int:
             try:
                 value = int(value)
