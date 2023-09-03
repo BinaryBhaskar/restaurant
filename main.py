df78e12479823452ea3dd058a600be3c27350298
Bhaskar Patel <117108968+BinaryBhaskar@users.noreply.github.com>
2023-09-03 05:33:56 +0000

    Added Ordering system

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #FlavourSync, a program originally made by Bhaskar Patel for his school project.
 
-import json, pytz
-from datetime import datetime
+import json
+import pytz
+from datetime import datetime as dt
 from tabulate import tabulate
 from res_library import get_input as ginput
 
@@ -12,7 +13,7 @@ with open("res_menu.json", encoding = 'utf-8') as f1:
 
 def check_weekend():
     india_timezone = pytz.timezone('Asia/Kolkata')
-    today = datetime.now(india_timezone).weekday()
+    today = dt.now(india_timezone).weekday()
     if today < 5:
         return False
     else:
@@ -22,13 +23,15 @@ def add_menu_item(menu_data):
     name = input("Enter the name of the new menu item: ")
     price = float(input("Enter the price of the new menu item: "))
     category = ginput("Enter the category of the new menu item (veg/non-veg/sweets/drinks): ",str,["veg", "non-veg", "sweets", "drinks"], True)
+    code = input("Enter the code of the new item")
     special = input("Is this a special item for weekends? (y/n): ").lower() == 'y'
 
     new_item = {
     "name": name,
     "price": price,
     "category": category,
-    "special": special    
+    "code": code,
+    "special": special
     }
 
     menu_data["menu"].append(new_item)
@@ -38,7 +41,7 @@ def add_menu_item(menu_data):
     with open('res_menu.json', encoding = 'utf-8') as f2:
         json.load(f2)
 
-def print_categorized_menu(menu_data,for_order = True):
+def print_categorized_menu(menu_data,for_order = False):
     categorized_menu = {"veg": [], "non-veg": [], "sweets": [], "drinks": []}
 
     for item in menu_data["menu"]:
@@ -60,19 +63,31 @@ def print_categorized_menu(menu_data,for_order = True):
     print(table)
 
     if for_order:
-        pass
+        start_order()
+
+def start_order():
+    order_list = []
+    menu_list = ['done']
+    for item in data["menu"]:
+        menu_list.append(item["code"].lower())
+    while True:
+        add_item = ginput("Enter the code of the dish you want to buy (Write 'done' to proceed): ", str, menu_list, True)
+        if add_item.lower() == 'done':
+            break
+        order_list.append(add_item.upper())
+        print(order_list)
 
 def home():
     chosen_menu = ginput("Enter what you want to see: \n'o' : Order Food\n't' : Track Order\n'm' : Show Menu\n'r' : Table Reservation\n'e' : Employee Management\n'i' : Restaurant Info\n",str,['o','e','i','m','t','r'],True)
     if chosen_menu == 'r':
         reserve()
     elif chosen_menu == 'm':
-        print_categorized_menu(data)
+        print_categorized_menu(data, False)
         home()
     elif chosen_menu == 'e':
         manage_employees()
     elif chosen_menu == 'o':
-        start_order()
+        print_categorized_menu(data, True)
     elif chosen_menu == 't':
         tracking()
     elif chosen_menu == 'i':
@@ -82,9 +97,6 @@ def reserve():
     pass
 
 def manage_employees():
-    pass
-
-def start_order():
     pass
 
 def tracking():
